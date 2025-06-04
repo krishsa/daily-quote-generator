9b7ec2377c9fabc4c5187b04215c772b9951557c
Krihnaveni,RajaLakshmi <krishnaveni.rl@gmail.com>
2025-06-03 22:25:28 -0500

    Simplify build configuration

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,13 +7,6 @@ export default defineConfig({
   base: '/daily-quote-generator/',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name].[hash][extname]',
-        chunkFileNames: 'assets/[name].[hash].js',
-        entryFileNames: 'assets/[name].[hash].js',
-      }
-    }
+    assetsDir: 'assets'
   }
 })
